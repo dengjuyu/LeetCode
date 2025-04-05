817a27a8050c680f0a254fc5c797df736b9ac983
유주현 <dbwngus08@naver.com>
2025-04-06 00:18:16 +0900

    Solve 35

--- src/easy/search_insert_position_35.py
+++ src/easy/search_insert_position_35.py
@@ -4,7 +4,22 @@ def search_insert(nums, target):
             return i
     return len(nums)
 
+def search_insert_binary_version(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+
 if __name__ == '__main__':
     nums = [1, 3, 5, 6]
     target = 5
     print(search_insert(nums, target))
+    print(search_insert_binary_version(nums, target))
